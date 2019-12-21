ba4344602c19d90ff231b93ccc8b99ca01c909fc
unknown <you@example.com>
2019-12-21 20:24:22 +0800

    主页整体布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="login">
-      <span>页面</span>
-  </div>
+ <!-- 设置外层边框 -->
+ <el-container>
+   <!-- 先左右布局，侧边栏容器 -->
+   <!-- 左侧 -->
+   <el-aside></el-aside>
+   <!-- 右侧再放置一个container因为右侧有是一个整体 -->
+   <!-- 右侧 -->
+   <el-container>
+     <!-- 上下布局 -->
+     <el-header>
+       <!-- 头部 -->
+     </el-header>
+     <el-main>
+      <!-- 主要区域 -->
+     </el-main>
+   </el-container>
+ </el-container>
 </template>
 
 <script>
